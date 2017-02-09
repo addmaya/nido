0db05182a0c44c0dba6f049fc4714e5cbadf33a2
Arthur Nakkaka <anakkaka@gmail.com>
2017-02-10 01:28:53 +0300

    update

--- single-project.php
+++ single-project.php
@@ -87,7 +87,7 @@
 			<h4 class="u-wrap">Reserve Plot(s)</h4>
 			<section class="u-wrap u-clear">
 				<div class="u-half">
-					<p class="s--reset">Use the Interactive below map to select and reserve plots on the Mutundwe Hill Estate. Plots marked red are sold. On Mobile, use 2 fingers to navgiate Map</p>
+					<p class="s--reset">Use the Interactive below map to select and reserve plots on the Mutundwe Hill Estate. Plots marked red are sold. On Mobile, use 2 fingers to navigate the Map</p>
 				</div>
 			</section>			
 			<div class="c-map u-clear">
