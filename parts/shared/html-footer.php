6c383532ccd749882a564ec88a1eeda030b2689a
Arthur Nakkaka <anakkaka@gmail.com>
2017-02-23 19:20:17 +0300

    update

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -1,6 +1,6 @@
 	<?php wp_footer(); ?>	
 	</div>
-	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build/vendors.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build/vendors.js?v0.185"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build/app.min.js?v0.185"></script>
 	<script>
 		var ajaxurl="<?php echo admin_url('admin-ajax.php'); ?>";
