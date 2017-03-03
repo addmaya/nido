37b120f6e544e647236403522a483ebdded572cf
Arthur Nakkaka <anakkaka@gmail.com>
2017-03-03 19:52:37 +0300

    update

--- functions.php
+++ functions.php
@@ -238,7 +238,7 @@
 	  remove_menu_page( 'edit-comments.php' );          
 	  remove_menu_page( 'themes.php' );                
 	  remove_menu_page( 'plugins.php' );         
-	  //remove_menu_page( 'users.php' );                
+	  remove_menu_page( 'users.php' );                
 	  remove_menu_page( 'tools.php' );
 	}
 	add_action( 'admin_menu', 'remove_menus' );
