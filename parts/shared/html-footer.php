cd2340a98c4e37271189c0b5a0fa403bc490c4b5
Addmaya <admin@addmaya.com>
2017-03-07 16:15:13 +0300

    update

--- parts/shared/html-footer.php
+++ parts/shared/html-footer.php
@@ -1,7 +1,7 @@
 	<?php wp_footer(); ?>	
 	</div>
-	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build/vendors.js?v0.194"></script>
-	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build/app.min.js?v0.194"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build/vendors.js?v0.195"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build/app.min.js?v0.195"></script>
 	<script>
 		var ajaxurl="<?php echo admin_url('admin-ajax.php'); ?>";
 		var posts_page = "<?php echo get_option( 'posts_per_page' ); ?>";
